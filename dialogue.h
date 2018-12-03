d270b8480840649b7cbab3a18948eaccdb5b3566
tsatwal <38673935+Taygh@users.noreply.github.com>
2018-12-03 15:50:26 -0800

    Add files via upload

---
+++ dialogue.h
@@ -0,0 +1,72 @@
+#pragma once
+
+//Purpose: Create stack for dialogue
+//
+//    Attributes:
+//        struct Node: creates linked list for dialogue, including
+//        a node constructor 
+//        Node *head: track head of linked list
+//
+//    Member Functions:
+//        Dialogue(): Preconditions: none,
+//        Postconditions: none (constructs stack)
+//        ~Dialogue(): Preconditions: stack exists, Postconditions:
+//        none (destroys stack)
+//        Dialogue(): Preconditions: a stack
+//        exists to be passed in, Postconditions: none (constructs
+//        second stack with attributes of passed in stack, copy
+//        constructor)
+//        Dialogue& operator=(): Preconditions:
+//        two stacks exist to be compared, Postconditions: none
+//        (sets the two stacks to have equal attributes,
+//        overloaded assignment operator)
+//        void push(): Preconditions: dialogue to push onto
+//        top of stack, Postconditions: none (adds dialogue to top
+//        of stack)
+//        string pop(): Preconditions: none,
+//        Posconditions: pops/removes dialogue at
+//        top of the stack, returns that dialogue
+//        string peak(): Preconditions: none.
+//        Postconditions: returns top of stack
+//        bool empty() const: Preconditions: none, Postconditions:
+//        none (checks if stack is empty)
+//        void copyReverse(): Preconditions: another Dialogue object,
+//        Postconditions: copy dialogue in reverse order from passed in object
+//        void dialoguePrint(): Preconditions: boolean representing what response
+//        the user chose (first/true or second/false), Postconditions: none
+//        (prints corresponding dialogue)
+//        void clear(): Preconditions: none, Postconditions: none 
+//        (pops off all dialogue to clear stack)
+//        void sleepMilliseconds(): Preconditions: integer for amount of milliseconds
+//        to wait, Postconditions: no return type, pauses print for "int n" time 
+
+class Dialogue
+{
+
+    public:
+        Dialogue();
+        ~Dialogue();
+        Dialogue(const Dialogue& other);
+        Dialogue& operator=(const Dialogue& other);
+
+        void push(std::string c);
+        std::string pop();
+        std::string peak();
+        bool empty() const;
+        void copyReverse(const Dialogue& other);
+		void dialoguePrint(bool decision);
+        
+  
+    private:
+
+        struct Node
+        {
+            std::string c;
+            Node *next;
+            Node(std::string c, Node *next = nullptr) : c(c), next(next) {}
+        };
+        Node *head;
+
+        void clear();
+		void sleepMilliseconds(int n);
+};
---
+++ starttext.cpp
@@ -0,0 +1,94 @@
+#include <iostream>
+#include <thread>
+#include "starttext.h"
+
+//Purpose: To animate a welcome message to the screen 
+//
+//    Attributes:
+//        	string lineOne: first line of animated welcome message
+//        	string lineTwo: second line of animated welcome message
+//        	string lineThree: third line of animated welcome message
+//        	string lineFour: fourth line of animated welcome message
+//        	string lineFive: fifth line of animated welcome message
+//        	string lineSix: sixth line of animated welcome message
+//
+//    Member Functions:
+//          StartText(): Preconditions: none, Postconditions: object created
+//          void display(): Preconditions: none (object already created implied),
+//          Postconditions: no return type, message animated to screen
+//          void sleepMilliseconds(): Preconditions: integer for amount of milliseconds
+//          to wait, Postconditions: no return type, pauses print for "int n" time 
+//          string switchLine(): Preconditions: integer representing next line needed
+//          for animating welcome message, Postconditions: line corresponding to 
+//          integer inputed
+
+StartText::StartText()
+{
+	lineOne = "___________              __      ________                        ___________              .__               ";
+	lineTwo = "\\__    ___/___ ___  ____/  |_   /  _____/_____    _____   ____   \\_   _____/ ____    ____ |__| ____   ____  ";
+	lineThree = "  |    |_/ __ \\\\  \\/  /\\   __\\ /   \\  ___\\__  \\  /     \\_/ __ \\   |    __)_ /    \\  / ___\\|  |/    \\_/ __ \\ ";
+	lineFour = "  |    |\\  ___/ >    <  |  |   \\    \\_\\  \\/ __ \\|  Y Y  \\  ___/   |        \\   |  \\/ /_/  >  |   |  \\  ___/ ";
+	lineFive = "  |____| \\___  >__/\\_ \\ |__|    \\______  (____  /__|_|  /\\___  > /_______  /___|  /\\___  /|__|___|  /\\___  >";
+	lineSix = "             \\/      \\/                \\/     \\/      \\/     \\/          \\/     \\//_____/         \\/     \\/ ";
+}
+
+void StartText::display()
+{
+	std::string current;
+
+	for (int i = 1; i < 7; i++)
+	{
+		current = switchLine(i);
+		int length = 0;
+
+		while (length < current.length())
+		{
+			std::cout << current[length];
+			sleepMilliseconds(20);
+			length++;
+		}
+
+		std::cout << std::endl;
+	}
+
+	std::cout << std::endl;
+}
+
+void StartText::sleepMilliseconds(int n) 
+{
+	std::this_thread::sleep_for(std::chrono::milliseconds(n));
+}
+
+std::string StartText::switchLine(int in)
+{
+	if (in == 1)
+	{
+		return lineOne;
+	}
+
+	if (in == 2)
+	{
+		return lineTwo;
+	}
+
+	if (in == 3)
+	{
+		return lineThree;
+	}
+
+	if (in == 4)
+	{
+		return lineFour;
+	}
+
+	if (in == 5)
+	{
+		return lineFive;
+	}
+
+	if (in == 6)
+	{
+		return lineSix;
+	}
+
+}
